0dc6d3137c0581ea156314dc80b75f264b5aa40e
master <seulli_hys@daum.net>
2021-01-15 17:03:34 +0900

    helloWorld

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,9 @@
+
 ReactDOM.render(
 	<h1>Hello, world!</h1>,
 	document.getElementById('root')
-);
-  
+  );
 
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+// const element = <h1>Hello, world</h1>;
+// ReactDOM.render(element, document.getElementById('root'));
+
