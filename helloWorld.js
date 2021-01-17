f67d3edbfdb3732d8e6b2b04b39250a41076041e
seulli <seulli_hys@daum.net>
2021-01-17 17:40:02 +0900

    react test

--- helloWorld.js
+++ helloWorld.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import {render} from 'react-dom';
+// import HelloWorld from './HelloWorld.js';
 
-ReactDOM.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('root')
-  );
+render(<HelloWorld/>, document.querySelector('#hellowWorld'));
+
+//
+// ReactDOM.render(
+// 	<h1>Hello, world!</h1>,
+// 	document.getElementById('root')
+//   );
 
 // const element = <h1>Hello, world</h1>;
 // ReactDOM.render(element, document.getElementById('root'));
 
+// return React.createElement(
+//   "h1",
+//   null,
+//   "Hello World!"
+// );
